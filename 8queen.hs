ba9c5e0333b6f42407387242f9a409caab885e8c
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2015-10-10 13:08:15 +0200

    [8queen] Solution with early pruning of tree.
    Gives < 1 minute for 12, 13 and 14

--- 8queen.hs
+++ 8queen.hs
@@ -1,22 +1,27 @@
 import System.Environment
-import Data.List (permutations)
 
 main :: IO ()
 main = do
   n <- (read . head) <$> getArgs
   print $ length (queens n)
 
-generate :: Int -> [[Int]]
-generate n = permutations [1..n]
-
 sameDiag :: Int -> [Int] -> Bool
 sameDiag try qs = any (\(colDist,q) -> abs (try - q) == colDist) $ zip [1..] qs
 
 queens :: Int -> [[Int]]
-queens n = filter test (generate n)
-    where test []         = True
-          test (q:qs)     = isSafe q qs && test qs
-          isSafe   try qs = not (sameDiag try qs)
+queens n = queens' n n
+
+queens' :: Int -> Int -> [[Int]]
+queens' _ 0 = [[]]
+queens' nTotal n = do
+  current <- queens' nTotal (n - 1)
+  col <- nextColumn current nTotal
+
+  return $ col:current
+
+nextColumn :: [Int] -> Int -> [Int]
+nextColumn current nTotal = filter (not . (\x -> elem x current || sameDiag x current)) [1..nTotal]
+
 
 {-
 Times:
@@ -28,10 +33,12 @@ n res t
 4 2   0.001
 5 10  0.001
 6 4   0.001
-7 40  0.002
-8 92  0.014
-9 352 0.116
-10 724 1.267
-11 2680 14.413
-12 --> killed after 2 minutes (but no ram excess)
+7 40  0.001
+8 92  0.003
+9 352 0.007
+10 724 0.028
+11 2680 0.142
+12 14200 0.877
+13 73712 5.276
+14 365596 34.633
 -}
