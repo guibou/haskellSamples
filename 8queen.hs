28f2051ca6c5172fc48c458896be2b51b0cdb44a
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2015-10-10 11:35:00 +0200

    [8queen] Add times

--- 8queen.hs
+++ 8queen.hs
@@ -13,3 +13,18 @@ queens n = filter test (generate n)
           test (q:qs)     = isSafe q qs && test qs
           isSafe   try qs = not (try `elem` qs || sameDiag try qs)
           sameDiag try qs = any (\(colDist,q) -> abs (try - q) == colDist) $ zip [1..] qs
+
+{-
+Times:
+
+n res t
+1 1   0.001
+2 0   0.001
+3 0   0.001
+4 2   0.001
+5 10  0.001
+6 4   0.007
+7 40  0.1
+8 92  2.554
+9 ----------> Unknown (stoped after 2 minutes and 10 Gb of ram)
+-}
