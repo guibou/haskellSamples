e676180ab5db6999ae2862cfc078508e54b5852e
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2015-10-10 11:10:08 +0200

    First Héléne version

---
+++ 8queen.hs
@@ -0,0 +1,12 @@
+main :: IO ()
+main = print $ length (queens 8)
+
+
+queens :: Int -> [[Int]]
+queens n = filter test (generate n)
+    where generate 0      = [[]]
+          generate k      = [q : qs | q <- [1..n], qs <- generate (k-1)]
+          test []         = True
+          test (q:qs)     = isSafe q qs && test qs
+          isSafe   try qs = not (try `elem` qs || sameDiag try qs)
+          sameDiag try qs = any (\(colDist,q) -> abs (try - q) == colDist) $ zip [1..] qs
